2d45d27ffe49f82882dcdf97cd87735578698045
김한별 <127176821+1ostar@users.noreply.github.com>
2023-12-07 22:00:55 +0900

    Update sample.c

--- 고프어쩌면최종/고프2어쩌면최종/ConsoleApplication2/sample.c
+++ 고프어쩌면최종/고프2어쩌면최종/ConsoleApplication2/sample.c
@@ -1,113 +1,115 @@
-//#include "jjuggumi.h"
-//#include "canvas.h"
-//#include "keyin.h"
-//#include <stdio.h>
-//
-//#define DIR_UP		0
-//#define DIR_DOWN	1
-//#define DIR_LEFT	2
-//#define DIR_RIGHT	3
-//
-//void sample_init(void);
-//void move_manual(key_t key);
-//void move_random(int i, int dir);
-//void move_tail(int i, int nx, int ny);
-//
-//int px[PLAYER_MAX], py[PLAYER_MAX], period[PLAYER_MAX];  // ∞¢ «√∑π¿ÃæÓ ¿ßƒ°, ¿Ãµø ¡÷±‚
-//
-//void sample_init(void) {
-//	map_init(15, 40);
-//	int x, y;
-//	for (int i = 0; i < n_player; i++) {
-//		// ∞∞¿∫ ¿⁄∏Æ∞° ≥™ø¿∏È ¥ŸΩ√ ª˝º∫
-//		do {
-//			x = randint(1, N_ROW - 2);
-//			y = randint(1, N_COL - 2);
-//		} while (!placable(x, y));
-//		px[i] = x;
-//		py[i] = y;
-//		period[i] = randint(100, 500);
-//
-//		back_buf[px[i]][py[i]] = '0' + i;  // (0 .. n_player-1)
-//	}
-//
-//	tick = 0;
-//}
-//
-//void move_manual(key_t key) {
-//	// ∞¢ πÊ«‚¿∏∑Œ øÚ¡˜¿œ ∂ß x, y∞™ delta
-//	static int dx[4] = { -1, 1, 0, 0 };
-//	static int dy[4] = { 0, 0, -1, 1 };
-//
-//	int dir;  // øÚ¡˜¿œ πÊ«‚(0~3)
-//	switch (key) {
-//	case K_UP: dir = DIR_UP; break;
-//	case K_DOWN: dir = DIR_DOWN; break;
-//	case K_LEFT: dir = DIR_LEFT; break;
-//	case K_RIGHT: dir = DIR_RIGHT; break;
-//	default: return;
-//	}
-//
-//	// øÚ¡˜ø©º≠ ≥ı¿œ ¿⁄∏Æ
-//	int nx, ny;
-//	nx = px[0] + dx[dir];
-//	ny = py[0] + dy[dir];
-//	if (!placable(nx, ny)) {
-//		return;
-//	}
-//
-//	move_tail(0, nx, ny);
-//}
-//
-//// 0 <= dir < 4∞° æ∆¥œ∏È ∑£¥˝
-//void move_random(int player, int dir) {
-//	int p = player;  // ¿Ã∏ß¿Ã ±ÊæÓº≠...
-//	int nx, ny;  // øÚ¡˜ø©º≠ ¥Ÿ¿Ωø° ≥ı¿œ ¿⁄∏Æ
-//
-//	// øÚ¡˜¿œ ∞¯∞£¿Ã æ¯¥¬ ∞ÊøÏ¥¬ æ¯¥Ÿ∞Ì ∞°¡§(π´«— ∑Á«¡ø° ∫¸¡¸)	
-//
-//	do {
-//		nx = px[p] + randint(-1, 1);
-//		ny = py[p] + randint(-1, 1);
-//	} while (!placable(nx, ny));
-//
-//	move_tail(p, nx, ny);
-//}
-//
-//// back_buf[][]ø° ±‚∑œ
-//void move_tail(int player, int nx, int ny) {
-//	int p = player;  // ¿Ã∏ß¿Ã ±ÊæÓº≠...
-//	back_buf[nx][ny] = back_buf[px[p]][py[p]];
-//	back_buf[px[p]][py[p]] = ' ';
-//	px[p] = nx;
-//	py[p] = ny;
-//}
-//
-//void sample(void) {
-//	sample_init();
-//
-//	system("cls");
-//	display();
-//	while (1) {
-//		// player 0∏∏ º’¿∏∑Œ øÚ¡˜¿”(4πÊ«‚)
-//		key_t key = get_key();
-//		if (key == K_QUIT) {
-//			break;
-//		}
-//		else if (key != K_UNDEFINED) {
-//			move_manual(key);
-//
-//		}
-//
-//		// player 1 ∫Œ≈Õ¥¬ ∑£¥˝¿∏∑Œ øÚ¡˜¿”(8πÊ«‚)
-//		for (int i = 1; i < n_player; i++) {
-//			if (tick % period[i] == 0) {
-//				move_random(i, -1);
-//			}
-//		}
-//		
-//		display();
-//		Sleep(10);
-//		tick += 10;
-//	}
-//}
+/*
+#include "jjuggumi.h"
+#include "canvas.h"
+#include "keyin.h"
+#include <stdio.h>
+
+#define DIR_UP		0
+#define DIR_DOWN	1
+#define DIR_LEFT	2
+#define DIR_RIGHT	3
+
+void sample_init(void);
+void move_manual(key_t key);
+void move_random(int i, int dir);
+void move_tail(int i, int nx, int ny);
+
+int px[PLAYER_MAX], py[PLAYER_MAX], period[PLAYER_MAX];  // Í∞Å ÌîåÎ†àÏù¥Ïñ¥ ÏúÑÏπò, Ïù¥Îèô Ï£ºÍ∏∞
+
+void sample_init(void) {
+	map_init(15, 40);
+	int x, y;
+	for (int i = 0; i < n_player; i++) {
+ 	// Í∞ôÏùÄ ÏûêÎ¶¨Í∞Ä ÎÇòÏò§Î©¥ Îã§Ïãú ÏÉùÏÑ±
+		do {
+			x = randint(1, N_ROW - 2);
+			y = randint(1, N_COL - 2);
+		} while (!placable(x, y));
+		px[i] = x;
+		py[i] = y;
+		period[i] = randint(100, 500);
+
+		back_buf[px[i]][py[i]] = '0' + i;  // (0 .. n_player-1)
+	}
+
+	tick = 0;
+}
+
+void move_manual(key_t key) {
+	// Í∞Å Î∞©Ìñ•ÏúºÎ°ú ÏõÄÏßÅÏùº Îïå x, yÍ∞í delta
+	static int dx[4] = { -1, 1, 0, 0 };
+	static int dy[4] = { 0, 0, -1, 1 };
+
+	int dir;  // ÏõÄÏßÅÏùº Î∞©Ìñ•(0~3)
+	switch (key) {
+	case K_UP: dir = DIR_UP; break;
+	case K_DOWN: dir = DIR_DOWN; break;
+	case K_LEFT: dir = DIR_LEFT; break;
+	case K_RIGHT: dir = DIR_RIGHT; break;
+	default: return;
+	}
+
+	// ÏõÄÏßÅÏó¨ÏÑú ÎÜìÏùº ÏûêÎ¶¨
+	int nx, ny;
+	nx = px[0] + dx[dir];
+	ny = py[0] + dy[dir];
+	if (!placable(nx, ny)) {
+		return;
+	}
+
+	move_tail(0, nx, ny);
+}
+
+// 0 <= dir < 4Í∞Ä ÏïÑÎãàÎ©¥ ÎûúÎç§
+void move_random(int player, int dir) {
+	int p = player;  // Ïù¥Î¶ÑÏù¥ Í∏∏Ïñ¥ÏÑú...
+	int nx, ny;  // ÏõÄÏßÅÏó¨ÏÑú Îã§ÏùåÏóê ÎÜìÏùº ÏûêÎ¶¨
+
+	// ÏõÄÏßÅÏùº Í≥µÍ∞ÑÏù¥ ÏóÜÎäî Í≤ΩÏö∞Îäî ÏóÜÎã§Í≥† Í∞ÄÏ†ï(Î¨¥Ìïú Î£®ÌîÑÏóê Îπ†Ïßê)	
+
+	do {
+		nx = px[p] + randint(-1, 1);
+		ny = py[p] + randint(-1, 1);
+	} while (!placable(nx, ny));
+
+	move_tail(p, nx, ny);
+}
+
+// back_buf[][]Ïóê Í∏∞Î°ù
+void move_tail(int player, int nx, int ny) {
+	int p = player;  // Ïù¥Î¶ÑÏù¥ Í∏∏Ïñ¥ÏÑú...
+	back_buf[nx][ny] = back_buf[px[p]][py[p]];
+	back_buf[px[p]][py[p]] = ' ';
+	px[p] = nx;
+	py[p] = ny;
+}
+
+void sample(void) {
+	sample_init();
+
+	system("cls");
+	display();
+	while (1) {
+		// player 0Îßå ÏÜêÏúºÎ°ú ÏõÄÏßÅÏûÑ(4Î∞©Ìñ•)
+		key_t key = get_key();
+		if (key == K_QUIT) {
+			break;
+		}
+		else if (key != K_UNDEFINED) {
+			move_manual(key);
+
+		}
+
+		// player 1 Î∂ÄÌÑ∞Îäî ÎûúÎç§ÏúºÎ°ú ÏõÄÏßÅÏûÑ(8Î∞©Ìñ•)
+		for (int i = 1; i < n_player; i++) {
+			if (tick % period[i] == 0) {
+				move_random(i, -1);
+			}
+		}
+		
+		display();
+		Sleep(10);
+		tick += 10;
+	}
+}
+*/
